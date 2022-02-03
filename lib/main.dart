d05e24eceff63ffcf63e6f1d0ea883329f5a1b75
rkdtnlzl <rkdtnlzl@naver.com>
2022-02-03 14:59:26 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chattingapp_ui/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chatting App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginSignupScreen(),
+    );
+  }
+}
